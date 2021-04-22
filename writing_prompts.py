c3ce9958b034cfa19d192434f15ed6156cd1dd58
muffin <squishysparrow7.7@gmail.com>
2021-04-22 22:35:12 +1200

    request function from jcchouinard.com

--- writing_prompts.py
+++ writing_prompts.py
@@ -7,6 +7,33 @@
 # https://old.reddit.com/r/WritingPrompts/top.json?sort=top&t=day&count=100
 
 import random
+import requests
+
+def get_reddit(subreddit,listing,limit,timeframe):
+    # Taken from https://www.jcchouinard.com/documentation-on-reddit-apis-json/
+
+    # subreddit = writingprompts
+    # listing = controversial, best, hot, new, random, rising, top
+    # limit = 100
+    # timeframe = hour, day, week, month, year, all
+
+    try:
+        base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?\
+                     limit={limit}&\
+                     t={timeframe}'
+        request = requests.get(base_url, headers = {'User-agent': 'writingprompts-yank'})
+    except:
+        print('An Error Occured')
+    return request.json()
+
+
+def process_prompts(listing, limit, timeframe)
+    posts = get_reddit('writingprompts',listing,limit,timeframe)
+
+    prompts = []
+
+    # Loop through the posts
+        # Add 'title' to prompts if flair is 'Writing Prompt'. Removing [WP] from it
 
 if __name__ == "__main__":
     # Code
