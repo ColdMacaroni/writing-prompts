f0b5ee055a0c81d92a45597aeff663778f9a27a9
muffin <squishysparrow7.7@gmail.com>
2021-04-22 22:27:01 +1200

    Initial commit

---
+++ writing_prompts.py
@@ -0,0 +1,13 @@
+#!/bin/python
+# Prints a random writing prompt from r/writingprompts
+# Takes the top 100 of today
+# TODO: Add options to select from
+
+# Sample api link
+# https://old.reddit.com/r/WritingPrompts/top.json?sort=top&t=day&count=100
+
+import random
+
+if __name__ == "__main__":
+    # Code
+    pass
