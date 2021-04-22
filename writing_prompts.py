7f587ff27d757bd071a909683eb4e3d84f9bef8b
muffin <squishysparrow7.7@gmail.com>
2021-04-22 23:01:44 +1200

    start on function to read wp from tmp file

--- writing_prompts.py
+++ writing_prompts.py
@@ -9,6 +9,7 @@
 import random
 import requests
 
+
 def get_reddit(subreddit,listing,limit,timeframe):
     # Taken from https://www.jcchouinard.com/documentation-on-reddit-apis-json/
 
@@ -18,27 +19,53 @@ def get_reddit(subreddit,listing,limit,timeframe):
     # timeframe = hour, day, week, month, year, all
 
     try:
-        base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?\
-                     limit={limit}&\
-                     t={timeframe}'
+        base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}'
+
         request = requests.get(base_url, headers = {'User-agent': 'writingprompts-yank'})
+
     except:
         print('An Error Occured')
+
     return request.json()
 
 
 def process_prompts(listing, limit, timeframe):
-    posts = get_reddit('writingprompts',listing,limit,timeframe)
+    posts = get_reddit('writingprompts', listing, limit, timeframe)
     prompts = []
 
+    # This is to avoid things like mod posts
     # link_flair_text
-    flair = 'Writing Prompt'
+    acceptable_flairs = ['Writing Prompt', 'Established Universe']
+
     # Loop through the posts
     for child in posts['data']['children']:
-        print(child)
-        print()
-        # Add 'title' to prompts if flair is 'Writing Prompt'. Removing [WP] from it
+        post = child['data']
+
+        if post['link_flair_text'] in acceptable_flairs:
+            prompts.append(post['title'].replace('[WP] ', ''))
+
+    return prompts
+
+
+def get_prompt(listing, limit, timeframe):
+
+    wp_tmp_file = '/tmp/writingprompts.txt'
+    try:
+        # Try to open the temp file with prompts
+        with open(wp_tmp_file, 'r') as txt:
+            prompts = txt.readlines()
+
+    except:
+        with open(wp_tmp_file, 'w+') as txt:
+            prompts = process_prompts(listing, limit, timeframe)
+
+            # Add current details
+            txt.write(' '.join(listing, limit, timeframe))
+            txt.write('\n')
+
+
+
 
 if __name__ == "__main__":
     # Code
-    print(process_prompts('top', '100', 'day'))
+    print(len(process_prompts('top', '100', 'day')))
